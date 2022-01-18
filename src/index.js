a3f3cdca7439933a5991fea73bbe76bd7ab71ae9
erikboren <erik.boren@gmail.com>
2022-01-18 22:06:09 +0100

    Webpack dist success!

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 /*jshint esversion: 6 */
+import './style.css';
+import empanadas1 from './empanadas1.png';
+import empanadas2 from './empanadas2.png';
+import empanadaPhoto from './delicious-typical-meat-empanada-argentina.jpg';
+
 const body = document.querySelector("body");
 const contentDiv = document.getElementById("content");
 
@@ -30,18 +35,23 @@ const blurDiv = function(){
 const logo = function(){
     const blurDiv = document.getElementById("blur-div");
     const logo = document.createElement("div");
-    const empanadas = [];
-    for(let i = 0; i < 2; i++){
-        const nbr = i +1;
-        empanadas[i] = document.createElement("img");
-        empanadas[i].setAttribute("src","empanadas"+nbr+".png");
-        empanadas[i].classList.add("logoPic");
-        logo.appendChild(empanadas[i]);
-    }
+    
+    const empanadas1pic = new Image();
+    empanadas1pic.src = empanadas1;
+    empanadas1pic.classList.add("logoPic");
+
+    const empanadas2pic = new Image();
+    empanadas2pic.src = empanadas2;
+    empanadas2pic.classList.add("logoPic");
+    
+    logo.appendChild(empanadas1pic);
+    logo.appendChild(empanadas2pic);
 
     const textDiv = document.createElement("div");
 
-    textDiv.innerHTML = "Mr. Elk's Empanadas";
+    textDiv.classList.add("logoText");
+
+    textDiv.textContent = "Mr. Elk's Empanadas";
 
     logo.setAttribute("id","logo");
 
@@ -52,7 +62,7 @@ const logo = function(){
 };
 
 const makeTextBox = function(){
-    element = document.createElement("div");
+    const element = document.createElement("div");
     element.classList.add("textBoxDiv");
     return element;
 };
@@ -69,20 +79,45 @@ const homePage = function(){
     textBox.appendChild(heading);
 
     const paragraph = document.createElement("p");
-    paragraph.innerHTML = "At Mr. Elk's Empanadas, Mr Elk takes great pride in serving ourcustomers authentic empanadas combined with sensations from the wast forrests of Sweden, Mr Elk's home country. It's a Spanish-Swedish fusion and it's all the rage!";
+    paragraph.innerHTML = "At Mr. Elk's Empanadas, Mr Elk takes great pride in serving our customers authentic empanadas combined with sensations from the wast forests of Sweden, Mr Elk's home country. It's a Spanish-Swedish fusion and it's all the rage!";
 
     textBox.appendChild(paragraph);
 
-    const image = document.createElement("img");
-    image.setAttribute("src","delicious-typical-meat-empanada-argentina.jpg");
-    image.setAttribute("id","empanadasPhoto");
+    const image = new Image();
+    image.src = empanadaPhoto;
+
+    image.setAttribute('id','empanadasPhoto');
 
     textBox.appendChild(image);
 
     blurDivElement.appendChild(textBox);
 };
 
+
+
+
 const menuPage = function(){
+    const menuItem = function(name,desc){
+        const item = document.createElement('div');
+        item.classList.add("menuItem");
+
+        const menuName = document.createElement("h2");
+        const menuDesc = document.createElement("p");
+
+        menuName.textContent=name;
+        menuDesc.textContent=desc;
+
+        item.appendChild(menuName);
+        item.appendChild(menuDesc);
+        return item;
+    };
+    
+    const menuList = [
+        ["Fir needle empanadas" , "Winner of Green Guide most innovative dish four years in a row. Fir needle from Mr. Elk's private forest"],
+        ["Herring stuffed empanadas", "Do you dare to try this Swedish-Spanish fusion?"],
+        ["Empanadas hamburger", "For the children and the mini Elks. All the goodies of a hamburger, stuffed into an empanada"]
+    ];
+
     contentDiv.innerHTML ="";
     const blurDivElement = blurDiv();
     
@@ -94,7 +129,11 @@ const menuPage = function(){
 
     textBox.appendChild(menuTitle);
 
-
+    menuList.forEach(dish =>{
+        console.log(dish[1]);
+        const item = menuItem(dish[0],dish[1]);
+        textBox.appendChild(item);
+    });
 
     blurDivElement.appendChild(textBox);
 };
